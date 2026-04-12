09cbc244eeb96bab3ca36641af2e1c17c5c2a31f
Alan Varghese <96782099+Alanvarghese-sbc@users.noreply.github.com>
2026-04-12 23:31:40 +0530

    Add runner-up score calculation
    Implement logic to find the runner-up score in a list of integers.

--- Basic/runner-up-score/solution_manual.py
+++ Basic/runner-up-score/solution_manual.py
@@ -1,1 +1,20 @@
 
+if __name__ == '__main__':
+    n = int(input())
+    arr = list(map(int, input().split()))
+    
+    max_val = arr[0]
+    
+    for i in arr:
+        if i > max_val:
+            max_val = i
+    
+    second_max = None
+    
+    for i in arr:
+        if i != max_val:
+            if second_max is None or i > second_max:
+                second_max = i
+            
+            
+    print(second_max)
