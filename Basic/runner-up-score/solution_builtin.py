82bb9ccd65665ca91ac6d403bd5998a6fd50fe54
Alan Varghese <96782099+Alanvarghese-sbc@users.noreply.github.com>
2026-04-12 23:31:09 +0530

    Simplify runner-up score calculation
    Refactor to use a more concise method for finding the runner-up score.

--- Basic/runner-up-score/solution_builtin.py
+++ Basic/runner-up-score/solution_builtin.py
@@ -1,1 +1,17 @@
 
+if __name__ == '__main__':
+    n = int(input())
+    arr = list(map(int, input().split()))
+    
+    unq_arr = list((set(arr)))
+    unq_arr.sort()
+    print(unq_arr[-2])
+    
+
+or
+
+if __name__ == '__main__':
+    n = int(input())
+    arr = list(map(int, input().split()))
+
+    print(sorted(set(arr))[-2]) 
