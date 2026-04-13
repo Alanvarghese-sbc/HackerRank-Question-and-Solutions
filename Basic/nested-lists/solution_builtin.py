11aa4b4358f5dbe5df65236935b6d1b8e15d3b44
Alan Varghese <96782099+Alanvarghese-sbc@users.noreply.github.com>
2026-04-13 22:53:38 +0530

    Update solution_builtin.py

--- Basic/nested-lists/solution_builtin.py
+++ Basic/nested-lists/solution_builtin.py
@@ -1,1 +1,16 @@
-
+if __name__ == '__main__':
+    
+    students = []
+    
+    for _ in range(int(input())):
+        name = input()
+        score = float(input())
+        students.append([name,score])
+        
+    second_lowest = sorted(list(set(score for name,score in students)))[1]
+    
+    names = [name for name, score in students if score == second_lowest ]    
+    names.sort()
+    
+    for name in names:
+        print(name)
