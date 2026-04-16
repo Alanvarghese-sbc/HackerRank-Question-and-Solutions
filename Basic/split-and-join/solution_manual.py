2a2a62ee0b2d3b5fbd7e7f8bf73df93c9fd44695
Alan Varghese <96782099+Alanvarghese-sbc@users.noreply.github.com>
2026-04-16 23:38:22 +0530

    Update solution_manual.py

--- Basic/split-and-join/solution_manual.py
+++ Basic/split-and-join/solution_manual.py
@@ -1,1 +1,14 @@
+def split_and_join(line):
+    words = line.split(" ")
+    
+    result = ""
+    for i in range(len(words)):
+        result += words[i]
+        if i != len(words) - 1:
+            result += "-"
+    
+    return result
 
+if __name__ == '__main__':
+    line = input()
+    print(split_and_join(line))
